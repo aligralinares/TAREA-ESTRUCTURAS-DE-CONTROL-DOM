b8a5ce050c930d0873682588e796f41acf965fcc
aligra <aligralinares@gmail.com>
2023-06-03 21:35:26 -0600

    Segundo Avance: Ejercicio 3

--- EJERCICIO 2/script.js
+++ EJERCICIO 2/script.js
@@ -7,5 +7,5 @@ const researchInput = document.getElementById ('researchScore');
 
 function getScore(){
     var finalScore = testInput.value * 0.20 + homeworkInput.value * 0.40 + attendanceInput.value * 0.10 + researchInput.value * 0.30;
-    alert(`Alumno: ${nameInput.value}\nCarnet: ${carnetInput.value}\nNota Final: ${finalScore}`)
-}
+    alert(`Alumno: ${nameInput.value}\nCarnet: ${carnetInput.value}\nNota Final: ${finalScore}`);
+};
--- EJERCICIO 3/script.js
+++ EJERCICIO 3/script.js
@@ -1,4 +1,26 @@
 const nameInput = document.getElementById('name');
 const salaryInput = document.getElementById('salary');
-const categotyInput = document.getElementById ('category');
+const categoryInput = document.getElementById ('categorySelect');
+
+function calculateSalary (){
+    let newSalary = 0;
+
+    switch (categoryInput.value) {
+        case 'A':
+            newSalary = salaryInput.value * 0.15;
+            break;
+        case 'B':
+            newSalary = salaryInput.value * 0.30;
+            break;    
+        case 'C':
+            newSalary = salaryInput.value * 0.10;
+            break;
+        case 'D':
+            newSalary = salaryInput.value * 0.20;
+            break;
+        default:
+            break;
+    };
+    document.write("Nombre del trabajador: " + nameInput.value + "<br>" + "Salario: $" + salaryInput.value + "<br>" + "Categoria: " + categoryInput.value + "<br>" + "Aumento salarial: $" + newSalary);
+};
 
